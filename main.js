b6f7669e4fbf489d519e06ed5523ea3b0e50ca36
msoh <journy002@naver.com>
2022-01-08 23:22:41 +0900

    update

--- main.js
+++ main.js
@@ -7,18 +7,18 @@ function callData() {
 function createHTMLString(item) {
   return `
         <li>
-            <img src='${item.image}' alt='${item.type}' />
+            <img src='${item.image}' alt='${item.type}'/>
             <span>${item.gender}, ${item.size}</span>
         </li>
     `;
 }
 
-function displayList(items) {
-  const dataList = document.querySelector(".items");
-  dataList.innerHTML = items.map((item) => createHTMLString(item)).join("");
+function displayItem(items) {
+  const itemBox = document.querySelector(".items");
+  itemBox.innerHTML = items.map((item) => createHTMLString(item)).join("");
 }
 
-function onButtonClick(event, items) {
+function onBtn(event, items) {
   const target = event.target.dataset;
   const key = target.key;
   const value = target.value;
@@ -26,16 +26,17 @@ function onButtonClick(event, items) {
   if (key === null || value === null) {
     return;
   }
+
   const filtered = items.filter((item) => item[key] === value);
-  displayList(filtered);
+  displayItem(filtered);
 }
 
 function setEventListener(items) {
   const buttons = document.querySelector(".buttons");
-  buttons.addEventListener("click", (event) => onButtonClick(event, items));
+  buttons.addEventListener("click", (event) => onBtn(event, items));
 }
 
 callData().then((items) => {
-  displayList(items);
+  displayItem(items);
   setEventListener(items);
 });
